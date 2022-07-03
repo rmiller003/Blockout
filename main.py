8b1dc3a71674a4769fc492a5152e931eed5c0a81
rmill <rmiller003@gmail.com>
2022-07-02 20:49:45 -0400

    Stage 1

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is Breakout-X developed in Python 3.9 by Robert Miller
+
+import pygame, sys, time
+from settings import *
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    def run(selfself):
+        last_time = time.time()
+        while True:
+
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # update window
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
