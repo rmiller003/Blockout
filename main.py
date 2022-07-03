242ce1a13834a09711cfe2a79e1226c17cdeb1d4
rmill <rmiller003@gmail.com>
2022-07-02 20:59:51 -0400

    Stage 2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Game:
         # general setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Breakout-X')
+        pygame.display.set_caption('Blockout')
 
     def run(selfself):
         last_time = time.time()
