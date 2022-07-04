0214684ee69f105cee0f215af30aecc7d5348298
rmill <rmiller003@gmail.com>
2022-07-04 18:34:44 -0400

    Stage 3

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Blockout')
 
-    def run(selfself):
+        # Background
+        self.bg = self.create_bg()
+
+    def create_bg(self):
+        bg_original = pygame.image.load('bg.png').convert()
+        scaled_bg = pygame.transform.scale(bg_original,(WINDOW_WIDTH, WINDOW_HEIGHT))
+        return scaled_bg
+
+    def run(self):
         last_time = time.time()
         while True:
 
@@ -26,10 +34,14 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            # update window
+            # Draw the Frame
+            self.display_surface.blit(self.bg,(0,0))
+
+
+            #update window
             pygame.display.update()
 
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+        game = Game()
+        game.run()
